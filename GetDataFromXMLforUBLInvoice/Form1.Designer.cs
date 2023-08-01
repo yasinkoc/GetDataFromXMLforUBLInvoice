74b90a5398f37dce3fbe6c0f4b51fbc4cb04c9b9
Yasin KOC <y.koc@daikin.com.tr>
2023-08-01 17:15:49 +0300

    Add project files.

---
+++ GetDataFromXMLforUBLInvoice/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace GetDataFromXMLforUBLInvoice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(587, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.memoEdit1.Location = new System.Drawing.Point(12, 12);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Properties.Appearance.Font = new System.Drawing.Font("Calibri", 11F);
+            this.memoEdit1.Properties.Appearance.Options.UseFont = true;
+            this.memoEdit1.Size = new System.Drawing.Size(565, 419);
+            this.memoEdit1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 443);
+            this.Controls.Add(this.memoEdit1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+    }
+}
+
---
+++ GetDataFromXMLforUBLInvoice/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GetDataFromXMLforUBLInvoice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Xml.XmlDocument dd = new System.Xml.XmlDocument();
+            dd.Load(@"C:\Users\Yasin KOC\Desktop\faturaorj.xml");
+            var deneme = dd.ChildNodes[0];
+
+            //foreach (System.Xml.XmlElement item in deneme.ChildNodes)
+            //{
+            //    string name = item.Name;
+            //    string value = "";
+            //    
+            //    if (item.ChildNodes.Count > 0)
+            //    {
+            //        if (item.ChildNodes.Count == 1)
+            //        {
+            //            value = item.ChildNodes[0].Value;
+            //        }
+            //        else
+            //        {
+            //            for (int i = 0; i < item.ChildNodes.Count; i++)
+            //            {
+            //                name = item.ChildNodes[i].Name;
+            //                value = item.ChildNodes[i].ChildNodes[0].Value;
+            //            }
+            //        }
+            //        
+            //    }
+            //}
+            memoEdit1.Text = "";
+            for (int i = 0; i < deneme.ChildNodes.Count; i++)
+            {
+                var item = deneme.ChildNodes[i];
+                if (item.Name.Substring(0,3) == "cac")
+                {
+                    int Level = 1;
+                    //memoEdit1.Text += item.Name + "\r\n";
+                    //if (item.Name == "cac:InvoiceLine")
+                    //{
+                    //
+                    //}
+
+                    string t = "";
+                    for (int j = 0; j < Level; j++)
+                    {
+                        t += "\t";
+                    }
+
+                    TxtDosyasinaYaz(t + item.Name);
+                    LookCbc((System.Xml.XmlElement)item, ref Level);
+                    //memoEdit1.Text += item.Name + "\r\n";
+                    TxtDosyasinaYaz(t + item.Name);
+                }
+                else if (item.Name.Substring(0, 3) == "cbc")
+                {
+                    //memoEdit1.Text += GetNameValueFromcbcXML((System.Xml.XmlElement)item) + "\r\n";
+                    TxtDosyasinaYaz(GetNameValueFromcbcXML((System.Xml.XmlElement)item));
+                }
+                else
+                {
+
+                }
+            }
+            MessageBox.Show("Kayıt İşlemi Başarılı!");
+        }
+
+        private string GetNameValueFromcbcXML(System.Xml.XmlElement pXmlElement)
+        {
+            return pXmlElement.Name + " => " + (pXmlElement.ChildNodes.Count == 0 ? "" : pXmlElement.ChildNodes[0].Value);
+            //return pXmlElement.Name;// + " => " + (pXmlElement.ChildNodes.Count == 0 ? "" : pXmlElement.ChildNodes[0].Value);
+        }
+        
+        private void LookCbc(System.Xml.XmlElement xmlElement, ref int pLevel)
+        {
+            for (int i = 0; i < xmlElement.ChildNodes.Count; i++)
+            {
+                System.Xml.XmlElement item = (System.Xml.XmlElement)xmlElement.ChildNodes[i];
+                if (item.Name.Substring(0, 3) == "cac")
+                {
+                    string t = "";
+                    if (i > 0 && xmlElement.ChildNodes.Count > i && xmlElement.ChildNodes[i - 1].Name.Substring(0, 3) == "cac")
+                    {
+
+                    }
+                    else
+                    {
+                        pLevel++;
+                    }
+                    
+                    if (pLevel > 0)
+                    {
+                        for (int j = 0; j < pLevel; j++)
+                        {
+                            t += "\t";
+                        }
+                    }
+                    TxtDosyasinaYaz(t + item.Name);
+                    LookCbc(item, ref pLevel);
+                    TxtDosyasinaYaz(t + item.Name);
+                }
+                else if (item.Name.Substring(0, 3) == "cbc")
+                {
+                    string t = "";
+                    for (int j = 0; j < pLevel + 1; j++)
+                    {
+                        t += "\t"; 
+                    }
+                    //memoEdit1.Text += "\t" + GetNameValueFromcbcXML(item) + "\n";
+                    TxtDosyasinaYaz(t + GetNameValueFromcbcXML(item));
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+        private void TxtDosyasinaYaz(string String)
+        {
+            string FilePath = @"C: \Users\Yasin KOC\Desktop\fatura.txt";
+
+            //FileStream fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Write);
+
+            if (File.Exists(FilePath))
+            {
+                //StreamWriter yaz = new StreamWriter(fileStream, Encoding.UTF8);
+                //yaz.WriteLine(String + Environment.NewLine);
+                //yaz.Close();
+                File.AppendAllText(FilePath, String + Environment.NewLine);
+                //fileStream.Close();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
